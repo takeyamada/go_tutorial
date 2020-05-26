3f2030b8639ebfba12b4b2d05aae643cd4663101
takeyamada <takeya0446@gmail.com>
2020-05-26 17:51:11 +0900

    c

--- sample.go
+++ sample.go
@@ -1,7 +1,11 @@
 package main
 import (
 	"fmt"
-	//"time"
+	"os"
+	"log"
+	"runtime/trace"
+	"context"
+	"time"
 )
 type (
 	Bean       int
@@ -59,32 +63,33 @@ func (cups Coffee) GroundBeans() GroundBean {
 	return GroundBean(20*cups) / GramGroundBeans
 }
 
-// お湯を沸かす
-func boil(water Water) HotWater {
-	//time.Sleep(400 * time.Millisecond)
-	return HotWater(water)
-}
-
-// コーヒー豆を挽く
-func grind(beans Bean) GroundBean {
-	//time.Sleep(200 * time.Millisecond)
-	return GroundBean(beans)
-}
-
-// コーヒーを淹れる
-func brew(hotWater HotWater, groundBeans GroundBean) Coffee {
-	//time.Sleep(1 * time.Second)
-	// 少ない方を優先する
-	cups1 := Coffee(hotWater / (1 * CupsCoffee).HotWater())
-	cups2 := Coffee(groundBeans / (1 * CupsCoffee).GroundBeans())
-	if cups1 < cups2 {
-		return cups1
-	}
-	return cups2
-}
-
 func main(){
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalln("Error:", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer trace.Stop()
+
+	_main()
+}
+
+func _main() {
+	// 作るコーヒーの数
+	done1 := make(chan bool)
+	done2 := make(chan bool)
 	const amountCoffee = 20 * CupsCoffee
+
+	ctx, task := trace.NewTask(context.Background(), "make coffee")
+	defer task.End()
 
 	// 材料
 	water := amountCoffee.Water()
@@ -95,28 +100,63 @@ func main(){
 
 	// お湯を沸かす
 	var hotWater HotWater
-	for water > 0 {
-		water -= 600 * MilliLiterWater
-		hotWater += boil(600 * MilliLiterWater)
-	}
+	go func(){
+		for water > 0 {
+			water -= 600 * MilliLiterWater
+			hotWater += boil(ctx, 600*MilliLiterWater)
+		}
+		done1<-true
+	}()
 	fmt.Println(hotWater)
 
 	// 豆を挽く
 	var groundBeans GroundBean
+	go func(){
 	for beans > 0 {
-		beans-=20
-		groundBeans+=grind(20 *GramBeans )
+		beans -= 20 * GramBeans
+		groundBeans += grind(ctx, 20*GramBeans)
 	}
+	done2<-true
+	}()
 	fmt.Println(groundBeans)
 
 	// コーヒーを淹れる
+	<-done1
+	<-done2
 	var coffee Coffee
-	cups := 9*CupsCoffee
+	cups := 4 * CupsCoffee
 	for hotWater >= cups.HotWater() && groundBeans >= cups.GroundBeans() {
-		hotWater-=cups.HotWater()
-		groundBeans-=cups.GroundBeans()
-		coffee+=brew(cups.HotWater(), cups.GroundBeans())
+		hotWater -= cups.HotWater()
+		groundBeans -= cups.GroundBeans()
+		coffee += brew(ctx, cups.HotWater(), cups.GroundBeans())
 	}
 
 	fmt.Println(coffee)
+}
+
+// お湯を沸かす
+func boil(ctx context.Context, water Water) HotWater {
+	defer trace.StartRegion(ctx, "boil").End()
+	time.Sleep(400 * time.Millisecond)
+	return HotWater(water)
+}
+
+// コーヒー豆を挽く
+func grind(ctx context.Context, beans Bean) GroundBean {
+	defer trace.StartRegion(ctx, "grind").End()
+	time.Sleep(200 * time.Millisecond)
+	return GroundBean(beans)
+}
+
+// コーヒーを淹れる
+func brew(ctx context.Context, hotWater HotWater, groundBeans GroundBean) Coffee {
+	defer trace.StartRegion(ctx, "brew").End()
+	time.Sleep(1 * time.Second)
+	// 少ない方を優先する
+	cups1 := Coffee(hotWater / (1 * CupsCoffee).HotWater())
+	cups2 := Coffee(groundBeans / (1 * CupsCoffee).GroundBeans())
+	if cups1 < cups2 {
+		return cups1
+	}
+	return cups2
 }
